436eb076a90cf2155370ea773840aa0d9adc1d76
golembiewskimarcin <golembiewskimarcin@gmail.com>
2025-01-19 22:03:31 +0100

    Initial commit

---
+++ first-projeckt-java-spring/src/main/java/pl/edu/vistula/first_projeckt_java_spring/FirstProjecktJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_projeckt_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjecktJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjecktJavaSpringApplication.class, args);
+	}
+
+}
---
+++ first-projeckt-java-spring/src/test/java/pl/edu/vistula/first_projeckt_java_spring/FirstProjecktJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_projeckt_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjecktJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
